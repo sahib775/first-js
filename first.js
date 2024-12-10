035a398930870067146ad0fb5dccc8ec0d85930d
sahib775 <ghulammurtaza7751618@gmail.com>
2024-12-10 06:32:46 +0000

    keywords in js

--- first.js
+++ first.js
@@ -1,1 +1,10 @@
-console.log("Murtaza")
+// console.log("Murtaza")
+let name = "ghulam murtaza"
+let age = 50
+let email = "ghulam@gmail.com"
+
+// for one output
+console.log(age)
+
+// for output in table
+console.table([name , age , email])
